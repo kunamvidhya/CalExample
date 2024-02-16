c9d54061ca17b2a4353eeb7e5899404222b31571
KISHOREREDDY0712 <navakishorekunam@gmail.com>
2024-02-16 10:12:10 +0530

    Firstcommit

---
+++ Cal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalExample
+{
+    public class Cal
+    {
+        public void Add(double n1, double n2)
+        {
+            Console.WriteLine("Result after addition: \t" + (n1 + n2));
+        }
+        public void Sub(double n1, double n2)
+        {
+            Console.WriteLine("Result after Subtraction: \t" + (n1 - n2));
+        }
+        public void Mul(double n1, double n2)
+        {
+            Console.WriteLine("Result after Mulitplication: \t" + (n1 * n2));
+        }
+        public void Div(double n1, double n2)
+        {
+            Console.WriteLine("Result after Division: \t" + (n1 / n2));
+        }
+    }
+}
+
